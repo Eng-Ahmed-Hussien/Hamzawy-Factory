41f523b9e9ab6e16227f3e13701461476b778b72
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2025-09-01 02:49:02 +0300

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,19 +5,20 @@ import morgan from "morgan";
 import router from "./routes/index.js";
 import { errorHandler, notFound } from "./middleware/error.js";
 
-
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://el-hamzawy-factory.vercel.app",
+  credentials: true,
+}));
+
 app.use(helmet());
 app.use(express.json());
 app.use(morgan("dev"));
 
-
 app.use("/api", router);
-
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 export default app;
